d8b24b1948d99edcfe2b1445b586e64210084be7
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-09-28 16:25:27 -0400

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import Logo from '../assets/logonew.png'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+
+        <div>
+          <img src={Logo} alt= "Temp Logo" style={{width: '50px'}}/> 
+        </div>
+        {/*Menu*/}
+        <div>
+                <ul className='flex'>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Skills</li>
+                  <li>Work</li>
+                  <li>Contact</li>
+
+                </ul>
+        </div>
 
 
+         {/*Hamburger*/}
+         <div className='hidden'>
+          <FaBars />
+         </div>
+
+        {/*Mobile Menu*/}
+        <ul className='hidden'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+        </ul>
+
+         {/*Social Icons*/}
+         <div></div>
 
     </div>
   )
