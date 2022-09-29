cd3388583a8c2b1e825681593a8e326a0163bd33
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-09-29 15:53:47 -0400

    fixed font issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,22 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+
+        {/*Container*/}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-pink-600'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Amit Narine</h1>
+            <h2>I am a Full Stack Developer,</h2>
+            <p>test block</p>
+            <div>
+                <button>View Work <HiArrowNarrowRight /></button>
+            </div>
+
+
+
+        </div>
+
 
 
 
