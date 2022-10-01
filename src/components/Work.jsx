306e8eb9f672a107299d576152f0f8dcf1996180
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-10-01 15:51:05 -0400

    working on background

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,7 +12,8 @@ const Work = () => {
             </div>
 
 
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            <div style={{backgroundImage: `url(${WorkImg})`}} 
+            className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
                 <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                    
                     {/* Hover Effects */}
