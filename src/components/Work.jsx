2fbc07913a865252cdd41024ac57ebcc32bf50d4
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-10-01 15:07:27 -0400

    created Work component and setting up container

---
+++ src/components/Work.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p>Work</p>
+                <p>// Check out some of my recent work</p>
+            </div>
+
+
+            <div>
+                <div>
+                    <div>
+                        <span></span>
+                        <div>
+                            <a href='/'>
+                                <button></button>
+                            </a>
+                            <a href='/'>
+                                <button></button>
+                            </a>
+                        </div>
+                        
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Work
