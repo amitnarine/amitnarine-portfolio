fe9bcc126f4295d1131745cde40d30d104ec916d
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-10-03 16:57:30 -0400

    made some css adjustments to look better

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ export const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/59a985ed-6980-48d9-bba3-0b6f7b4270e0" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
+            <div className='pt-20'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - amitnarine@yahoo.com</p>
             </div>
