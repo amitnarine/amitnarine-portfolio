dc72cd04ba2aca1b38b8731f8313d605258e2e25
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-10-04 01:49:00 -0400

    padding adjustments

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -21,7 +21,7 @@ export const Skills = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-[#A238Ca]'>Skills</p>
                 <p className='py-4'>// These are the technologies I've worked with</p>
             </div>
-            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-2 text-center py-8'>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-2 text-center py-4'>
                 <div className='border-4  border-[#A238Ca]/50 hover:scale-110 duration-500 pt-4'>
                     <img className='w-20 mx-auto' src={JAVA} alt="Java icon" />
                     <p className='my-4'>JAVA</p>
