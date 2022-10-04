1922bcbe5ecb22c4236b90c27255481aea18ba77
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-10-04 11:32:06 -0400

    fixing comments for warnings

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ export const Contact = () => {
         <form method='POST' action="https://getform.io/f/59a985ed-6980-48d9-bba3-0b6f7b4270e0" className='flex flex-col max-w-[600px] w-full'>
             <div className='pt-20'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#A238Ca] text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - amitnarine@yahoo.com</p>
+                <p className='text-gray-300 py-4'> Submit the form below or shoot me an email - amitnarine@yahoo.com</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name'></input>
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email'></input>
